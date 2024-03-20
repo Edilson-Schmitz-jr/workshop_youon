94b489d645ede8976e1e1c699f1ec3a0d289093e
Edilson Schmitz Junior <116024288+Edilson-Schmitz-jr@users.noreply.github.com>
2024-03-20 18:41:34 +0000

    Novo teste

--- script.js
+++ script.js
@@ -3,4 +3,5 @@
 var i = setInterval(function () {
     clearInterval(i);
     document.getElementById("load_screen").classList.add("active");
+    document.querySelector('.video_class_welc').play();
 }, 3000);
