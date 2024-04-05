bc811a1a598b836d52f313ba16afdcd410cf4822
Edilson Schmitz Junior <116024288+Edilson-Schmitz-jr@users.noreply.github.com>
2024-04-05 12:26:52 +0000

    Abertura em vídeo com foto opacity

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ var i = setInterval(function () {
     document.getElementById("load_screen").classList.add("active");
     document.querySelector('.video_class_welc').play();
 }, 3000);
+
+var u = setInterval(function () {
+    clearInterval(i);
+    document.querySelector('.video_class_welc').classList.add("active");
+    document.querySelector('.img_plano_de_fundo').classList.add("active");
+    document.querySelector('.video_class_welc').pause();
+}, 10000);
