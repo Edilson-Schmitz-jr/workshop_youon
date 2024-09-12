1eb09860046daf2be1dd51195b1908ae966dcb88
Edilson Schmitz Junior <116024288+Edilson-Schmitz-jr@users.noreply.github.com>
2024-09-12 17:33:23 +0000

    12/09 javascript

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var i = setInterval(function () {
     clearInterval(i);
     document.getElementById("load_screen").classList.add("active");
     document.querySelector('.video_class_welc').play();
-}, 2500);
+}, 1700);
 
 // var u = setInterval(function () {
 //     clearInterval(i);
